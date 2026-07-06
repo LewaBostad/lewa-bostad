a185e1e0f39c99e019ba1092d27e180f55ad7372
SebastianKullander9 <sebastian.kullander@protonmail.com>
2026-07-06 15:31:33 +0200

    hide footer on contact page

--- components/ui/SiteShell.tsx
+++ components/ui/SiteShell.tsx
@@ -1,15 +1,19 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import { ConsentProvider } from "@/components/ui/consent/ConsentContext";
 import CookieBanner from "@/components/ui/consent/CookieBanner";
 import Footer from "@/components/ui/footer/Footer";
 
 export default function SiteShell({ children }: { children: React.ReactNode }) {
+    const pathname = usePathname();
+    const hideFooter = pathname === "/kontakt";
+
     return (
         <ConsentProvider>
             {children}
             <CookieBanner />
-            <Footer />
+            {!hideFooter && <Footer />}
         </ConsentProvider>
     );
 }
